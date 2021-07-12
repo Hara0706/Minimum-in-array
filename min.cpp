c596232f63308ce38f194aecad3a9a960d125aee
Harapriya Mohapatra <73417004+Hara0706@users.noreply.github.com>
2021-07-12 17:37:35 +0530

    Create min.cpp

---
+++ min.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    int arr[5] = {1,5,6,2,4};
+    int min = a[0];
+    for(int i=0; i<5; i++)
+    {
+        if(a[i]<min) min = a[i];
+    }
+    cout<<min;
+	
+	return 0;
+}
